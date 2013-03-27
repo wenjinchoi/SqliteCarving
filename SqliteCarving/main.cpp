43c70ec2e3e555f8d961227316aa30c79a26f2d8
wenjinchoi <wenjin.choi@gmail.com>
2013-03-27 21:27:28 +0800

    Initial Commit

---
+++ SqliteCarving/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SqliteCarving
+//
+//  Created by Wenjin Choi on 3/27/13.
+//  Copyright (c) 2013 wenjin choi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
