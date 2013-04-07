795ee4bdc041a250ff4117048b587d66d4981a60
wenjinchoi <wenjin.choi@gmail.com>
2013-04-08 00:44:11 +0800

    Move test function to main()

--- SqliteCarving/main.cpp
+++ SqliteCarving/main.cpp
@@ -7,15 +7,58 @@
 //
 
 #include <iostream>
+#include <algorithm>
+
+#include "SqliteFileParser.h"
 #include "CellParser.h"
+
+using namespace std;
+
+void testCellParser()
+{
+    const unsigned char testdata[] = {
+        0x2B, 0x01, 0x12, 0x00, 0x01, 0x17, 0x00, 0x04,
+        0x00, 0x01, 0x01, 0x01, 0x00, 0x00, 0x1D, 0x00,
+        0x01, 0x01, 0x01, 0x01, 0x01, 0x31, 0x30, 0x30,
+        0x38, 0x36, 0x50, 0xE2, 0x43, 0x20, 0x00, 0xFF,
+        0x01, 0x52, 0x65, 0x63, 0x6F, 0x72, 0x64, 0x20,
+        0x31, 0x00, 0x00, 0x00, 0x00
+    };
+    
+    build_map();
+    
+    int length = sizeof(testdata)/sizeof(unsigned char);
+    vec_uchar vtd;
+    for (int i = 0; i < length; ++i) {
+        vtd.push_back(testdata[i]);
+    }
+    
+    cout << "Test Data: " << endl;
+    copy(vtd.begin(), vtd.end(), ostream_iterator<int>(cout, " "));
+    cout << endl << endl;
+    
+    parseTableLeafCell(vtd);
+}
+
+void testSqliteFileParser()
+{
+    SqliteFileParser fparser = SqliteFileParser();
+    fparser.setInputFile("/Users/wenjinchoi/Desktop/mmssms.db");
+    
+    int pageSize = fparser.pageSize();
+    cout << "Page Size: " << pageSize << endl;
+    
+    std::vector<unsigned char> thePage = fparser.pageAt(2);
+    copy(thePage.begin(), thePage.end(),
+         ostream_iterator<unsigned char>(cout, " "));
+}
+
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    // testParseVarint();
-    testo();
-    // std::cout << "Hello, World!\n";
+    testCellParser();
+    testSqliteFileParser();
+    
     return 0;
 }
 
